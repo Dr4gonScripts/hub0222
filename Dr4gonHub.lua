dbcdbe25718c00e1cfcf64e01fb4cafe81dd9219
Dr4gonScripts <oiranzinho00@gmail.com>
2025-06-06 17:42:50 -0300

    Dr4gonHub.lua

--- Dr4gonHub.lua
+++ Dr4gonHub.lua
@@ -98,5 +98,69 @@ BFTab:AddButton({
     end
 })
 
--- Inicializar a interface
+Tab:AddButton({
+	Name = "Ro Hub",
+	Callback = function()
+      		_G.settings = {
+    autoLoadConfig = false, -- self explanatory
+    joinTeam = "Pirates" -- "Pirates" or "Marines"
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RO-HUB-CODEX/RO-HUB/refs/heads/main/bloxfruits.lua"))()
+  	end    
+})
+
+Tab:AddButton({
+	Name = "W Azure",
+	Callback = function()
+      		getgenv().Team = "Marines" getgenv().AutoLoad = false --Will Load Script On Server Hop loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
+  	end    
+})
+
+Tab:AddButton({
+	Name = "Tsou Hub",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/Tsuo7/TsuoHub/main/Tsuoscripts"))()
+  	end    
+})
+
+Tab:AddButton({
+	Name = "Banana Free",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/kimprobloxdz/Banana-Free/refs/heads/main/Protected_5609200582002947.lua.txt"))()
+  	end    
+})
+
+local Tab = Window:MakeTab({
+	Name = "🌱 Grow a Garden",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Tab:AddSection({
+	Name = "Scipts - Auto farm & Dupe"
+})
+
+Tab:AddButton({
+	Name = "Speed X Hub",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+  	end    
+})
+
+Tab:AddButton({
+	Name = "No-Lag hub",
+	Callback = function()
+      		loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
+  	end    
+})
+
+Tab:AddButton({
+	Name = "OFF",
+	Callback = function()
+      		print("button pressed")
+  	end    
+})
+
+
 OrionLib:Init()
